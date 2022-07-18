07a0d72e3304b1d792ab3b3b0c3f4644f40d0424
chloemk <minsung.chloe.kim@gmail.com>
2022-07-18 14:44:23 +0900

    feat: add Scaffold and appbar as it changes to main screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -7,19 +7,24 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meals!'),
       ),
-      children: dummyCategories
-          .map(
-            (categoryData) => CategoryItem(
-                title: categoryData.title, color: categoryData.color),
-          )
-          .toList(),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: dummyCategories
+            .map(
+              (categoryData) => CategoryItem(
+                  title: categoryData.title, color: categoryData.color),
+            )
+            .toList(),
+      ),
     );
   }
 }
