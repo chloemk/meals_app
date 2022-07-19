061cb4d5fd03ae9a1dbd941e31f6423f1299b973
chloemk <minsung.chloe.kim@gmail.com>
2022-07-19 15:36:32 +0900

    feat: implement show image which user selected

---
+++ lib/screens/meal_detail_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  static const routeName = '/meal-detail';
+
+  const MealDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final mealId = ModalRoute.of(context)!.settings.arguments as String;
+    // 유저가 선택한 음식
+    final selectedMeal = dummyMeals.firstWhere((meal) => meal.id == mealId);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          mealId,
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(selectedMeal.imageUrl, fit: BoxFit.fill),
+          )
+        ],
+      ),
+    );
+  }
+}
