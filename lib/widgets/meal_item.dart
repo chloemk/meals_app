09c7423741259fd7527d089ad0d0d60e466d1852
chloemk <minsung.chloe.kim@gmail.com>
2022-07-19 14:17:04 +0900

    feat: add Positioned widget to title

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -45,9 +45,33 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.black54,
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 )
               ],
-            )
+            ),
           ],
         ),
       ),
