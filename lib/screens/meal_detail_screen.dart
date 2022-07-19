1de0e255983c476fc8e0e593d22e4d4a6f9e0a04
chloemk <minsung.chloe.kim@gmail.com>
2022-07-19 15:40:19 +0900

    fix: change title of Appbar when select meals

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -15,7 +15,7 @@ class MealDetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          mealId,
+          selectedMeal.title,
         ),
       ),
       body: Column(
