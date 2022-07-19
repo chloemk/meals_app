14b0b31012112a1079db13dec633a78c93f2eef9
chloemk <minsung.chloe.kim@gmail.com>
2022-07-19 15:50:42 +0900

    feat: change style of ingredients list

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -20,10 +20,38 @@ class MealDetailScreen extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          Container(
+          SizedBox(
             height: 300,
             width: double.infinity,
             child: Image.network(selectedMeal.imageUrl, fit: BoxFit.fill),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            height: 150,
+            width: 300,
+            child: ListView.builder(
+              itemBuilder: (context, index) => Card(
+                color: Theme.of(context).colorScheme.secondary,
+                child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    child: Text(selectedMeal.ingredients[index])),
+              ),
+              itemCount: selectedMeal.ingredients.length,
+            ),
           )
         ],
       ),
